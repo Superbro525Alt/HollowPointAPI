658e46efcfeb9edc3759afa6510955764d7f792c
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-09-27 11:23:34 +0800

    start.sh - use nodejs v16

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
-apt install node
-apt install npm
+sudo apt remove nodejs -y
+sudo apt purge nodejs -y
 
-npm install
+sudo apt install npm -y
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+source ~/.bashrc
+
+nvm install v16.14.0 -y
+
+npm install -y
 
 node ./src/index.js
